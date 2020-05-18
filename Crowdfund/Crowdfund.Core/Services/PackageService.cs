80891a454d1e7c90d30e711b85e9f450b80e30e1
azaridak <azaridak@gmail.com>
2020-05-18 21:45:59 +0300

    update package service

--- Crowdfund/Crowdfund.Core/Services/PackageService.cs
+++ Crowdfund/Crowdfund.Core/Services/PackageService.cs
@@ -10,54 +10,68 @@ namespace Crowdfund.Core.Services
 {
     public class PackageService : IPackageService
     {
-       private IProjectService projectService;
+        private IProjectService projectService;
         private CrowdfundDbContext dbContext;
 
         public PackageService(IProjectService projectservice, CrowdfundDbContext context)
         {
-            this.projectService= projectservice;
+            this.projectService = projectservice;
             this.dbContext = context;
         }
 
         public Package CreatePackage(CreatePackageOptions options)
         {
-             if (options == null || options.ProjectId == null) 
+            if (options == null || options.ProjectId == null)
             {
                 return null;
             }
+
             var project = dbContext
                 .Set<Project>()
                 .Where(p => p.ProjectId == options.ProjectId)
                 .SingleOrDefault();
 
-            if (project == null) 
+            if (project == null)
             {
                 return null;
             }
 
-            var package = new Package()
-            {
-                PackageId = options.PackageId
-            };
+            var package = new Package();
 
             if (!string.IsNullOrWhiteSpace(options.Description))
             {
-               Description = options.Description;
+                package.Description = options.Description;
             }
 
-            if (options.Price != null)
+            if (options.Price > 0M)
             {
                 package.Price = (decimal)options.Price;
             }
 
             project.Packages.Add(package);
-
-            return context_.SaveChanges() > 0 ? package : null;
+            return dbContext.SaveChanges() > 0 ? package : null;
         }
 
         public bool DeletePackage(int PackageId)
         {
-            throw new NotImplementedException();
+            //dbContext.Remove(dbContext.Set<Package>().Find(PackageId));
+
+            try
+            {
+                dbContext.Remove(dbContext.Set<Package>()
+                                              .Where(p => p.PackageId == PackageId)
+                                              .SingleOrDefault());
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            if (dbContext.SaveChanges() > 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public IQueryable<Package> SearchUser(SearchPackageOptions options)

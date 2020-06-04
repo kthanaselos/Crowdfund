4546d3f55df66979dc90a2f3fbb4b8130bde9dce
azaridak <azaridak@gmail.com>
2020-06-04 21:14:17 +0300

    update of home page

--- Crowdfund/Crowdfund.Web/Controllers/HomeController.cs
+++ Crowdfund/Crowdfund.Web/Controllers/HomeController.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Crowdfund.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Crowdfund.Web.Models;
+using Crowdfund.Core.Services.Options;
+using Crowdfund.Core.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Crowdfund.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        IProjectService projectService;
+        public HomeController(ILogger<HomeController> logger, IProjectService prjService)
         {
             _logger = logger;
+            projectService = prjService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(projectService.SearchProject(new SearchProjectOptions())
+            .Include(p => p.Media)
+            .Include(p => p.User)
+            .ToList());         
         }
 
         public IActionResult Privacy()
